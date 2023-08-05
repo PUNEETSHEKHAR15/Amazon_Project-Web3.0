282756658cacb7f890531dc6b3458ee550e5b8f1
PUNEET SHEKHAR <shekharpuneet19@gmail.com>
2023-08-05 14:20:16 +0530

    Dappazon contract and test.js is upadated with some tests on test.js file regarding deploying contract and owner testing

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item{
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256=>Item) public items;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /**
+     * List Product => save the items to the struct and map the items with mappping
+     * Buy Product 
+     * Withdraw the funds 
+     */
+
+
+    function listProduct() public payable (
+        uint256 _id ,
+        string memory _name,
+        string memory _category,
+        string memory _image ,
+        uint256 _cost ,
+        uint256 _rating,
+        uint256 _stock, 
+    ) {
+
+        Item memory item = Item(_id ,
+        _name,
+        _category,
+        _image,
+        _cost,
+        _rating,
+        _stock
+        )
+
+    // save the item to the blockchain with the mapping 
+    //mapping[id]=>instance of that stuct item  
+    items[_id]=item ;
+        
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,63 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe("Dappazon", () => {
+describe('Dappazon', async () => {
+  let dappazon
+  let buyer
+  beforeEach(async () => {
+    // there are total 20 accounts and depoyer and buyer are entered inside the array 
+    // at 1st and 2nd positions it will fetch starting 2 accounts
+    [buyer] = await ethers.getSigners()
+    // console.log(deployer, buyer.address)
+    // console.log((await ethers.getSigners()).length)
+    // Deploy the smart contract 
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
 
+  })
+
+  describe("Deployments", () => {
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(buyer.address)
+    })
+
+    // it("has a name", async () => {
+    //   const name = await dappazon.name()
+    //   expect(name).to.equal("Dappazon")
+    // })
+
+
+  })
 })
+
+
+/**
+ * insert Await keyword in all the places which has to be must  exexuted which 
+ * deploy the contrct 
+ * getSigners
+ * deploy 
+ * get contratfactoy 
+ * checking it functions
+ */
+
+/**
+ * Error Number 1
+ * [
+  Dappazon
+    Deployments
+      1) Sets the owner
+
+
+  0 passing (1s)
+  1 failing
+
+  1) Dappazon
+       Deployments
+         Sets the owner:
+     TypeError: dappazon.owner is not a function
+      at Context.<anonymous> (test/Dappazon.js:24:29)
+      at processImmediate (node:internal/timers:466:21)
+ ]
+  this is due to the visibilty of th eowner is not set (public)
+
+ */
